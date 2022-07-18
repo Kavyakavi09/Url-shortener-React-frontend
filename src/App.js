a4cd1b081a0e86ab30ff88bd37aace2412a471d1
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-07-18 08:26:35 +0530

    hashrouter applied

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './pages/Header';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ForgotPass from './pages/ForgetPass';
@@ -14,7 +14,7 @@ import Activate from './pages/Activate';
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Header />
         <Routes>
           <Route path='/' element={<Home />} />
@@ -24,7 +24,7 @@ function App() {
           <Route path='/sign-up-activate' element={<Activate />} />
           <Route path='/forgot-password' element={<ForgotPass />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
